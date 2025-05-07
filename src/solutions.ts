1234e371a00c2a809bc4aad14c9a70a38bd81b11
unknown <sr0589071@gmail.com>
2025-05-07 19:25:01 +0600

    Initialize TypeScript project with basic structure

---
+++ src/solutions.ts
@@ -0,0 +1,5 @@
+//Problem-1-solution
+
+export function formatString(input:string, toUpper: boolean = true): string{
+    return toUpper ? input.toUpperCase() : input.toLowerCase();
+}
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+//Problem-1-solution
+
+import { formatString } from "./solutions";
+
+console.log(formatString("Hello"));
+console.log(formatString("Hello",true));
+console.log(formatString("Hello", false));
